40f46ff6cb26de3461295c65edbcd1b36345acce
magruu <mfailla@ethz.ch>
2021-05-18 14:45:28 +0200

    updated javascript. Issues with notifications

--- Application/ESP32_TEM_Holder/data/script.js
+++ Application/ESP32_TEM_Holder/data/script.js
@@ -2,8 +2,8 @@
 /** Global variables **/ 
 
 // Notifications to be sent 
-var notification_msg_setting_position = '<div class="notification is-warning" id="notification"> <h3> Setting Holder Position ... </h3><progress class="progress is-medium is-dark" max="100">45%</progress></div>';
-var notification_msg_position_set = '<div class="notification is-success" id="notification"><h3 class="has-text-white"> Holder Position set!</h3></div>';
+var notification_msg_setting_position = '<div class="notification is-warning" id="noti_pos_setting"> <h3> Setting Holder Position ... </h3><progress class="progress is-medium is-dark" max="100">45%</progress></div>';
+var notification_msg_position_set = '<div class="notification is-success" id="noti_pos_set"><h3 class="has-text-white"> Holder Position set!</h3></div>';
 
 // Holder Position Status
 var position_set = false;
@@ -27,9 +27,9 @@ function slider_change(){
 
   output.innerHTML = slider.value;
 
-  if(this.value > (pos_3+pos_2)/2){
+  if(slider.value > (pos_3+pos_2)/2){
     probe_pos_3.checked = true;
-  } else if(this.value < ((pos_1+pos_2)/2)){
+  } else if(slider.value < ((pos_1+pos_2)/2)){
     probe_pos_1.checked = true;
   } else {
     probe_pos_2.checked = true;
@@ -90,43 +90,51 @@ function slider_plus_minus_btn(plus_minus) {
     default:
       break;
   }
-  output.innerHTML = slider.value;
-  arrow_position();
+
+  slider_change();
 }
 
 //holder position and send toast/notification
 function set_holder_position_btn(){
   var button = document.getElementById('button_holder_set');
+  var value = document.getElementById('sliderWithValue').value; // slider value
 
   button.className += " is-loading";
-  addNotifcation('notification_setting_position', notification_msg_setting_position);
-  var value = document.getElementById('sliderWithValue').value;
+  addNotification('notification_setting_position', notification_msg_setting_position);
+
+  //document.getElementById("notification").classList.add('notification_msg_setting_position');
+
   console.log(value);
-  webSocket.send(value);
+  webSocket.send(value); // send slider value to backend
 
-  if(position_set == true){
-    addNotifcation('notification_setting_position', notification_msg_position_set);
-  }
-
-  setTimeout(function(){button.classList.remove("is-loading");},4500);
+  // setTimeout(function(){button.classList.remove("is-loading");},4500);
 
 }
 
 // add notification
-function addNotifcation(id, msg) {
+function addNotification(id, msg) {
   var notification_setting = document.getElementById(id);
   const  div = document.getElementById(id);
   div.innerHTML += msg;
-
-  newdiv = document.getElementById("notification");
-  setTimeout(function(){
-   newdiv.style.opacity = '0';
-   newdiv.addEventListener('transitionend', () => newdiv.remove());
-
-  }, 4000);
 }
 
-// switch functionality
+// function addNotification(id, msg, msg_id){
+//   var myStyle = document.getElementById('myStyle').sheet;
+//   myStyle.insertRule('#'+msg_id+'{transition: opacity 0.5s;}');
+//   newdiv = document.getElementById(id);
+//   newdiv.innerHTML += msg;
+//   document.getElementById("notification").setAttribute("id", msg_id);
+// }
+
+function removeNotification(msg_id){
+  let newdiv = document.getElementById(msg_id);
+  newdiv.style.opacity = '0';
+  newdiv.addEventListener('transitionend', () => {newdiv.remove()});
+}
+
+
+
+//** switch functionality */ 
 // change to Live View Mode
 function switch_Live_Mode(){
 var switch_Live_Mode = document.getElementById('switch_Live_Mode');
